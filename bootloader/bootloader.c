a896b4784fca8bfb6b98e63d6cf92a933c20555d
Akshay Srivatsan <srivatsan@cs.stanford.edu>
2023-03-17 11:31:29 -0700

    Adjust log levels

--- bootloader/bootloader.c
+++ bootloader/bootloader.c
@@ -14,7 +14,7 @@ extern const struct payload payload;
 
 void kmain() {
   {
-    INFO("uart init\n");
+    TRACE("uart init\n");
     uart_init(2000000);
 
     gpio_set_function(14, GPIO_UART);
@@ -25,22 +25,21 @@ void kmain() {
   }
   INFO("bootloader init\n");
   {
-    INFO("psram init\n");
+    TRACE("psram init\n");
     psram_init();
-    INFO("psram check\n");
+    TRACE("psram check\n");
     volatile u32 *psram = (volatile u32 *)0x50000000;
     put32(psram, 0xdeadbeef);
     u32 x = get32(psram);
-    INFO("read back 0x%x\n", x);
+    DEBUG("read back 0x%x from psram\n", x);
     if (x != 0xdeadbeef) {
       ERROR("psram init failed!\n");
       while(1);
     }
     INFO("psram init success\n");
   }
-  INFO("timer init\n");
+  TRACE("timer init\n");
   timer_init(1000000);
-  INFO("have timer = %d\n", timer_read());
 
   INFO("magic = 0x%x\n", payload.magic);
   INFO("addr = 0x%lx\n", payload.entry);
@@ -54,7 +53,7 @@ void kmain() {
   } else {
     INFO("valid magic: 0x%x == %d\n", payload.magic, payload.magic);
   }
-  INFO("copying %d bytes of code to address %p\n", payload.size, payload.entry);
+  DEBUG("copying %d bytes of code to address %p\n", payload.size, payload.entry);
   memcpy((void *)payload.entry, payload.data, payload.size);
 
   uint32_t cksum = crc32((void *)payload.entry, payload.size);
@@ -66,9 +65,8 @@ void kmain() {
     INFO("valid checksum: 0x%x\n", cksum);
   }
 
-  INFO("branching to code\n");
+  TRACE("branching to code\n");
   void (*fn)(void) = (void (*)(void))payload.entry;
-  INFO("0x%x\n", *(volatile u32*)fn);
   fn();
   WARN("code returned to bootloader\n");
 }
